70064b8cce833f78e7875664d61d3f5b2053d4c1
ngoduc <ngoducnk@gmail.com>
2019-10-03 09:28:54 +0700

    doan so

---
+++ sortarray/javascript.js
@@ -0,0 +1,10 @@
+
+var array = [-3,5,1,3,2,10];
+while(true){
+    var x =prompt("nham number:");
+for (let i=0; i<array.length;i++){
+    if(array[i]==x){
+        alert("bing bong");
+    }
+    }
+}
